7c3e10036ddad14ff145bd044b8c1d85b2d05ca8
Aarya Tiwari <aarya.tiwari@somaiya.edu>
2024-08-09 11:04:43 +0530

    Added functionality to View Records in Profile

--- client/src/pages/ChatPDF.jsx
+++ client/src/pages/ChatPDF.jsx
@@ -1,17 +1,12 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import {
-    Drawer,
     Button,
-    Typography,
-    IconButton,
 } from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
-import { GiHamburgerMenu } from "react-icons/gi";
 import { FaFilePdf, FaUpload } from "react-icons/fa";
-import { useDropzone } from "react-dropzone";
-import ChatUI from './ChatUI';
 import axios from 'axios';
 import FormData from 'form-data';
+import ChatUI from './ChatUI';
 import Appbar from '../components/appbar/Appbar';
 import { useUser } from '@clerk/clerk-react';
 
@@ -29,26 +24,31 @@ const ChatPDF = () => {
         setChatter(false);
         setUploadedFile(null);
         setSubmit(false);
+        setFileName("");
+        setSourceId('');
     }
 
-    const onDrop = async (acceptedFiles) => {
-        console.log('Dropped files:', acceptedFiles);
-        setFileName(acceptedFiles[0].name);
-        const file = acceptedFiles[0];
-        const formData = new FormData();
-        formData.append('file', file);
+    const handleFileChange = async (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            console.log('Selected file:', file);
+            setFileName(file.name);
 
-        const responseFile = await axios.post('https://api.chatpdf.com/v1/sources/add-file', formData, {
-            headers: {
-                'x-api-key': import.meta.env.VITE_CHATPDF_API_KEY,
-                'Content-Type': 'multipart/form-data',
-            },
-        });
+            const formData = new FormData();
+            formData.append('file', file);
 
-        console.log("Source ID:", responseFile.data.sourceId);
-        setUploadedFile(file);
-        setSourceId(responseFile.data.sourceId);
-        setSubmit(true);
+            const responseFile = await axios.post('https://api.chatpdf.com/v1/sources/add-file', formData, {
+                headers: {
+                    'x-api-key': import.meta.env.VITE_CHATPDF_API_KEY,
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+
+            console.log("Source ID:", responseFile.data.sourceId);
+            setUploadedFile(file);
+            setSourceId(responseFile.data.sourceId);
+            setSubmit(true);
+        }
     };
 
     const handleSummarize = () => {
@@ -59,64 +59,60 @@ const ChatPDF = () => {
         setChatter(true);
     }
 
-
-    const { getRootProps, getInputProps } = useDropzone({ 
-        onDrop: onDrop,
-        accept: 'application/pdf',
-        maxFiles: 1,
-        multiple: false,
-        maxSize: 32 * 1024 * 1024,
-    });
-
     return (
         <>{
-            aiType === '' ? (<>
+            aiType === '' ? (
                 <div className='flex flex-col gap-10 md:gap-16 m-10 font-inter items-center justify-center h-[80vh] md:mx-64'>
                     <Button className='text-color1 bg-color3 font-inter' fullWidth onClick={() => setAiType("chat")}>Chat Using Report PDF</Button>
                     <Button className='text-color1 bg-color3 font-inter' fullWidth onClick={handleSummarize}>MedMatch Concierge</Button>
                 </div>
-            </>) : aiType === 'chat' && (<>
-                {(chatter ? (<ChatUI sourceId={{ sourceId }} fileName={{ fileName }} />) : (
-                    <>
-                        <div className='flex flex-col gap-10 m-10 font-inter'>
-                            <div className='flex flex-col gap-2'>
-                                <h1 className='text-2xl text-color1'>Upload a report</h1>
-                                <p className='text-sm text-gray-600'>Upload a report to get a summary or chat with the bot</p>
-                            </div>
-                            <div {...getRootProps()} className="flex items-center justify-center w-full">
-                                <label
-                                    htmlFor="dropzone-file"
-                                    className="flex flex-col items-center justify-center w-full h-64 p-4 text-white border-2 border-gray-600 border-dashed rounded-xl"
-                                >
-                                    {uploadedFile ? (
-                                        <div className="flex flex-col items-center justify-center gap-3 pt-5 pb-6">
-                                            <FaFilePdf size={40} />
-                                            <p className="mb-2 text-sm ">
-                                                <span className="font-semibold">{uploadedFile.name}</span>
-                                            </p>
-                                        </div>
-                                    ) : (
-                                        <div className="flex flex-col items-center justify-center gap-6 pt-5 pb-6">
-                                            <FaUpload size={60} />
-                                            <div className='flex flex-col gap-2 text-center'>
-                                                <p className="text-sm">DRAG AND DROP PDF FILE HERE</p>
-                                                <p className="text-sm">PDF FILE OF REPORT (MAX - 32MB)</p>
-                                            </div>
-                                        </div>
-                                    )}
-                                    <input id="dropzone-file" {...getInputProps()} className="hidden" />
-                                </label>
-                            </div>
-                            <div className='flex justify-between w-full'>
-                                <Button className='text-color1 bg-color3 font-inter' disabled={!submit} onClick={handleReset}>Reset</Button>
-                                <Button className='text-color1 bg-color3 font-inter' disabled={!submit} onClick={handleChat}>Chat</Button>
-                            </div>
+            ) : aiType === 'chat' && (
+                chatter ? (
+                    <ChatUI sourceId={{ sourceId }} fileName={{ fileName }} />
+                ) : (
+                    <div className='flex flex-col gap-10 m-10 font-inter'>
+                        <div className='flex flex-col gap-2'>
+                            <h1 className='text-2xl text-color1'>Upload a report</h1>
+                            <p className='text-sm text-gray-600'>Upload a report to get a summary or chat with the bot</p>
                         </div>
-                    </>
-                ))}
-            </>)
+                        <div className="flex items-center justify-center w-full">
+                            <label
+                                htmlFor="upload-file"
+                                className="flex flex-col items-center justify-center w-full h-64 p-4 text-white border-2 border-gray-600 border-dashed rounded-xl"
+                            >
+                                {uploadedFile ? (
+                                    <div className="flex flex-col items-center justify-center gap-3 pt-5 pb-6">
+                                        <FaFilePdf size={40} />
+                                        <p className="mb-2 text-sm ">
+                                            <span className="font-semibold">{uploadedFile.name}</span>
+                                        </p>
+                                    </div>
+                                ) : (
+                                    <div className="flex flex-col items-center justify-center gap-6 pt-5 pb-6">
+                                        <FaUpload size={60} />
+                                        <div className='flex flex-col gap-2 text-center'>
+                                            <p className="text-sm">CLICK TO UPLOAD PDF FILE</p>
+                                            <p className="text-sm">PDF FILE OF REPORT (MAX - 32MB)</p>
+                                        </div>
+                                    </div>
+                                )}
+                                <input
+                                    id="upload-file"
+                                    type="file"
+                                    accept="application/pdf"
+                                    onChange={handleFileChange}
+                                    className="hidden"
+                                />
+                            </label>
+                        </div>
+                        <div className='flex justify-between w-full'>
+                            <Button className='text-color1 bg-color3 font-inter' disabled={!submit} onClick={handleReset}>Reset</Button>
+                            <Button className='text-color1 bg-color3 font-inter' disabled={!submit} onClick={handleChat}>Chat</Button>
+                        </div>
+                    </div>
+                )
+            )
         }
-
             <Appbar />
         </>
     );
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,100 +1,135 @@
-import React, { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import { Button, IconButton, Typography, Spinner } from '@material-tailwind/react';
-import { FaUpload } from 'react-icons/fa';
-import { Input, Select, Label, Option } from '@material-tailwind/react';
-import Appbar from '../components/appbar/Appbar';
-import { useContext } from 'react';
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { Button, Typography } from "@material-tailwind/react";
 import { MdDocumentScanner } from "react-icons/md";
-import SahyogCard from '../components/Sahyogcard/SahyogCard';
-import { UserProfile, useUser } from '@clerk/clerk-react';
-import { db } from '../components/FirebaseSDK';
-import { collection, setDoc, getDoc } from 'firebase/firestore';
-import { useNavigate } from 'react-router-dom';
-import { dark } from '@clerk/themes';
-
-
-const ProfileData = ({ label, content, onChange }) => {
-
-    return (
-        <div className='flex flex-col gap-2 '>
-            <Typography color='white' className='text-lg font-bold text-blue-500 font-inter'>
-                {label}
-            </Typography>
-            (
-            <Input
-                type='text'
-                value={content}
-                className='text-white outline-none'
-                onChange={(e) => onChange(label.toLowerCase(), e.target.value)}
-            />
-            )
-        </div>
-    );
-};
+import SahyogCard from "../components/Sahyogcard/SahyogCard";
+import { UserProfile, useUser } from "@clerk/clerk-react";
+import { db } from "../components/FirebaseSDK";
+import { doc, getDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { dark } from "@clerk/themes";
+import Appbar from "../components/appbar/Appbar";
 
 const Profile = () => {
-    const { user } = useUser();
-    const navigate = useNavigate();
+  const { user } = useUser();
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState(null);
+  const [uploadedMedicalDetails, setUploadedMedicalDetails] = useState(false);
+  const [uploadedHealthInsurance, setUploadedHealthInsurance] = useState(false);
+  const [medicalRecordsUrl, setMedicalRecordsUrl] = useState(null);
+  const [healthInsuranceUrl, setHealthInsuranceUrl] = useState(null);
 
-    useEffect(() => {
-        console.log(user);
-    }, []);
+  const getMedicalRecordFile = async () => {
+    const userDocRef = doc(db, "users", user.id);
+    const docSnap = await getDoc(userDocRef);
+    if (docSnap.exists()) {
+      const url = docSnap.data().medicalRecords;
+      setMedicalRecordsUrl(url);
+    }
+  };
 
-    const initialProfileData = {
-        name: user?.fullName,
-        email: user?.emailAddresses[0].emailAddress,
-        age: '',
-        emergencyContact: '',
+  const getHealthInsuranceFile = async () => {
+    const userDocRef = doc(db, "users", user.id);
+    const docSnap = await getDoc(userDocRef);
+    if (docSnap.exists()) {
+      const url = docSnap.data().healthInsurance;
+      setHealthInsuranceUrl(url);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const userDocRef = doc(db, "users", user.id);
+      const docSnap = await getDoc(userDocRef);
+      if (docSnap.exists()) {
+        setUserData(docSnap.data());
+        if (docSnap.data().medicalRecords) {
+          setUploadedMedicalDetails(true);
+          await getMedicalRecordFile();
+        }
+        if (docSnap.data().healthInsurance) {
+          setUploadedHealthInsurance(true);
+          await getHealthInsuranceFile();
+        }
+      }
     };
+    if (user) {
+      fetchUserData();
+    }
+  }, [user]);
 
-    const [error, setError] = useState(null);
-
-    const [profileData, setProfileData] = useState(initialProfileData);
-
-    const handleChange = (field, value) => {
-        setProfileData((prevData) => ({
-            ...prevData,
-            [field]: value,
-        }));
-    };
-
-    return (
-        <>
-            <div className='flex flex-col gap-10 my-10 mx-7 md:mx-60'>
-                <motion.div
-                    className='flex flex-col gap-2'
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.5 }}
+  return (
+    <>
+      <div className="flex flex-col gap-10 my-10 mx-7 md:mx-60">
+        <motion.div
+          className="flex flex-col gap-2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <Typography color="white" className="text-2xl font-bold font-inter">
+            <u>My Profile</u>
+          </Typography>
+        </motion.div>
+        <div className="flex flex-col items-center gap-10">
+          <div className="flex flex-col gap-3">
+            <SahyogCard
+              name={userData?.name}
+              number={userData?.phone}
+              age={userData?.age}
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-5 ">
+            {medicalRecordsUrl ? (
+              <a href={medicalRecordsUrl} target="_blank" rel="noopener noreferrer">
+                <Button size="sm" color="green" className="text-md">
+                  View Medical Records
+                </Button>
+              </a>
+            ) : (
+              !uploadedMedicalDetails && (
+                <Button
+                  size="sm"
+                  color="blue"
+                  className="text-md"
+                  onClick={() => navigate("/medicaldetails")}
                 >
-                    <Typography color='white' className='text-2xl font-bold font-inter'>
-                        <u>My Profile</u>
-                    </Typography>
-                </motion.div>
-                <div className='flex flex-col items-center gap-10'>
-                    <div className='flex flex-col gap-3'>
-                        <SahyogCard name={profileData.name} number={profileData.emergencyContact} age={profileData.age} />
-                    </div>
-                    <div className='grid grid-cols-2 gap-5 '>
-                        <Button size='sm' color='blue' className='text-md' onClick={() => navigate("/medicaldetails")}>
-                            Upload Medical Details
-                        </Button>
-                        <Button size='sm' color='blue' className='text-md' onClick={() => navigate("/healthinsurance")}>
-                            Upload Health Insurance
-                        </Button>
-                    </div>
-                    <UserProfile appearance={{
-                        baseTheme: dark,
-                        elements: {
-                            card: 'margin-0'
-                        }
-                    }} />
-                </div>
-            </div>
-            <Appbar />
-        </>
-    );
+                  Upload Medical Details
+                </Button>
+              )
+            )}
+            {healthInsuranceUrl ? (
+              <a href={healthInsuranceUrl} target="_blank" rel="noopener noreferrer">
+                <Button size="sm" color="green" className="text-md">
+                  View Health Insurance
+                </Button>
+              </a>
+            ) : (
+              !uploadedHealthInsurance && (
+                <Button
+                  size="sm"
+                  color="blue"
+                  className="text-md"
+                  onClick={() => navigate("/healthinsurance")}
+                >
+                  Upload Health Insurance
+                </Button>
+              )
+            )}
+          </div>
+          <UserProfile
+            appearance={{
+              baseTheme: dark,
+              elements: {
+                card: "margin-0",
+              },
+            }}
+          />
+        </div>
+      </div>
+      <Appbar />
+    </>
+  );
 };
 
 export default Profile;
