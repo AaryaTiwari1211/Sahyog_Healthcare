2e85155dd7b9d983f43c5daa59f1054ef6675da5
Aarya Tiwari <aarya.tiwari@somaiya.edu>
2024-08-09 20:39:09 +0530

    Added some text in Login Signup pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,38 +1,42 @@
-
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { SignIn } from '@clerk/clerk-react'
+import { SignIn } from "@clerk/clerk-react";
 import { dark } from "@clerk/themes";
 import { useUser } from "@clerk/clerk-react";
 import { db } from "../components/FirebaseSDK";
 import { collection, getDocs, setDoc, getDoc, doc } from "firebase/firestore";
 
 const Login = () => {
-    const navigate = useNavigate();
-    const user = useUser();
-    useEffect(() => {
-        if (user.user) {
-            navigate("/landing");
-        }
-    }, []);
+  const navigate = useNavigate();
+  const user = useUser();
+  useEffect(() => {
+    if (user.user) {
+      navigate("/landing");
+    }
+  }, []);
 
-    return (
-        <div className="flex flex-col justify-center items-center">
-            <span>Welcome to Sahyog</span>
-            <span>Please Login in to Continue</span>
-            <SignIn path="/login" appearance={{
-                baseTheme: dark,
-                elements: {
-                    rootBox: "w-full h-screen flex flex-col justify-center items-center gap-6",
-                }
-            }}
-                redirectUrl="/landing"
-                forceRedirectUrl="/landing" // Redirects to landing page if user is already logged in
-                signUpUrl="/signup"
-                signupForceRedirectUrl="/landing"
-            />
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center mt-20 w-full">
+      <div className="flex flex-col w-full justify-center items-center gap-2">
+        <span className="text-white text-3xl font-bold ">Welcome to Sahyog</span>
+        <span className="text-white text-xl font-bold">Please Login in to Continue</span>
+      </div>
+      <SignIn
+        path="/login"
+        appearance={{
+          baseTheme: dark,
+          elements: {
+            rootBox:
+              "w-full mt-20 flex flex-col justify-center items-center gap-6",
+          },
+        }}
+        redirectUrl="/landing"
+        forceRedirectUrl="/landing" // Redirects to landing page if user is already logged in
+        signUpUrl="/signup"
+        signupForceRedirectUrl="/landing"
+      />
+    </div>
+  );
 };
 
 export default Login;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import { SignUp } from '@clerk/clerk-react'
-import { dark } from '@clerk/themes'
+import React from "react";
+import { SignUp } from "@clerk/clerk-react";
+import { dark } from "@clerk/themes";
 
 const Signup = () => {
-    return (
-        <div>
-            <SignUp
-                path="/signup"
-                redirectUrl="/landing"
-                routing="path"
-                appearance={{
-                    baseTheme: dark,
-                    elements: {
-                        rootBox: "w-full h-screen flex flex-col justify-center items-center gap-6",
-                    }
-                }}
-                signInUrl='/login'
-            />
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col justify-center items-center mt-20 w-full">
+      <div className="flex flex-col w-full justify-center items-center gap-2">
+        <span className="text-white text-3xl font-bold ">
+          Welcome to Sahyog
+        </span>
+        <span className="text-white text-xl font-bold">
+          Please Signup to Continue
+        </span>
+      </div>
+      <SignUp
+        path="/signup"
+        redirectUrl="/landing"
+        routing="path"
+        appearance={{
+          baseTheme: dark,
+          elements: {
+            rootBox:
+              "w-full mt-20 flex flex-col justify-center items-center gap-6",
+          },
+        }}
+        signInUrl="/login"
+      />
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
